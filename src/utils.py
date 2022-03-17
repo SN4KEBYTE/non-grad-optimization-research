f0f414133034a8527454df12c3c6337b79c45e89
Timur Kasimov <t.kasimov@aventica.ru>
2022-03-17 16:47:16 +0700

    Added random utils and optimizers factory

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,14 @@
+import random
+from itertools import product
+from typing import (
+    Generator,
+    Type,
+)
+
+import numpy as np
+import torch
 from torch import nn
+from torch.optim import Optimizer
 
 
 def init_weights(
@@ -20,3 +30,35 @@ def init_weights(
     elif isinstance(module, nn.LayerNorm):
         module.bias.data.zero_()
         module.weight.data.fill_(1.0)
+
+
+def make_optimizers(
+    optim: Type[Optimizer],
+    params,
+    **kwargs,
+) -> Generator:
+    for k, v in kwargs.items():
+        if not isinstance(v, list):
+            kwargs[k] = [v]
+
+    keys, values = zip(*kwargs.items())
+
+    for comb in [dict(zip(keys, v)) for v in product(*values)]:
+        yield (
+            optim(
+                params,
+                **comb,
+            ),
+            f'{optim.__name__} {", ".join(f"{k}={v}" for k, v in comb.items())}'
+        )
+
+
+def seed_everything(
+    seed: int,
+) -> None:
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
